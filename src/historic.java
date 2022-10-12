173e5efffdfc1524ff10f82a8d98db778421c59a
Alexandre Leclerc <leclercalex3@gmail.com>
2022-10-12 14:30:09 -0700

    send function

---
+++ src/historic.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class historic {
+    private JPanel mainPanel;
+    private JTable historicTable;
+    private JPanel tabelPanel;
+    private JButton showButton;
+    private JButton button2;
+}
