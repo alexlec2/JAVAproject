75822b93dc1051c4cbf16b1129d41a3c5f893b71
Alexandre Leclerc <leclercalex3@gmail.com>
2022-10-12 14:24:42 -0700

    send function

--- src/addOrder.java
+++ src/addOrder.java
@@ -2,6 +2,7 @@ import javax.lang.model.element.Element;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -112,5 +113,37 @@ public final class addOrder extends javax.swing.JFrame{
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void resultSetToTableModel(ResultSet rs, JTable table) throws SQLException {
+        //Create new table model
+        DefaultTableModel tableModel = new DefaultTableModel();
+
+        //Retrieve meta data from ResultSet
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        //Get number of columns from meta data
+        int columnCount = metaData.getColumnCount();
+
+        //Get all column names from meta data and add columns to table model
+        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++){
+            tableModel.addColumn(metaData.getColumnLabel(columnIndex));
+        }
+
+        //Create array of Objects with size of column count from meta data
+        Object[] row = new Object[columnCount];
+
+        //Scroll through result set
+        while (rs.next()){
+            //Get object from column with specific index of result set to array of objects
+            for (int i = 0; i < columnCount; i++){
+                row[i] = rs.getObject(i+1);
+            }
+            //Now add row to table model with that array of objects as an argument
+            tableModel.addRow(row);
+        }
+
+        //Now add that table model to your table and you are done :D
+        table.setModel(tableModel);
     }
 }
