de8c736904ec5deb8067b853b954c3ecbcc65341
Alexandre Leclerc <leclercalex3@gmail.com>
2022-10-12 00:49:49 -0700

    clean + phrase conclu order en corus

--- src/addOrder.java
+++ src/addOrder.java
@@ -1,13 +1,24 @@
+import javax.lang.model.element.Element;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public final class addOrder extends javax.swing.JFrame{
     private JPanel addOrderMainPanel;
     private JButton returnButton;
-    private JSpinner spinner1;
     private JTabbedPane tabbedPane1;
-    private JLabel welcomelbl;
+    private JPanel dessertPanel;
+    private JPanel dishPanel;
+    private JPanel appetizerPanel;
+    private JPanel drinkPanel;
+    private JLabel lblTotal;
+    private JPanel panelTabs;
+    int count = 0;
+    Statement statement;
 
     public static void main(String[] args) {
         new addOrder(1);
@@ -19,6 +30,9 @@ public final class addOrder extends javax.swing.JFrame{
         setSize(400,800);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+
+
         returnButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -26,5 +40,77 @@ public final class addOrder extends javax.swing.JFrame{
                 dispose();
             }
         });
+    }
+
+    private void displayDishTable(String query1, JPanel panel) throws SQLException {
+        ResultSet result1 = statement.executeQuery(query1);
+
+        while (result1.next()){
+            JPanel panelTemp = new JPanel();
+            panelTemp.add(new JLabel(result1.getString(2)));
+            panelTemp.add(new JLabel("(Restant: "+result1.getString(5)+")"));
+            SpinnerModel value = new SpinnerNumberModel(0, 0, Integer.parseInt(result1.getString(5)), 1);
+            JSpinner spinner = new JSpinner(value);
+            spinner.addChangeListener(new ChangeListener() {
+                  @Override
+                  public void stateChanged(ChangeEvent e) {
+                      Component component = panelTemp.getComponent(2);
+                      count += (int) ((JSpinner)component).getValue();
+                      //JOptionPane.showMessageDialog(null, ((JSpinner)component).getValue());
+                      JSpinner s = (JSpinner) e.getSource();
+                      JOptionPane.showMessageDialog(null, s.getValue().toString());
+                      lblTotal.setText("The total number of command is : " + count);
+                  }
+            });
+            panelTemp.add(spinner);
+            panel.add(panelTemp);
+        }
+        result1.close();
+    }
+
+    private void createUIComponents() {
+        //connection database
+        try {
+            String ip = "212.227.188.100";
+            String port = "2022";
+            String user = "javaRestaurant";
+            String password = "$ja3va!R3st5auran5t.926";
+            String database_name = "db_restaurant";
+            Connection connection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + database_name + "", user, password);
+
+            statement = connection.createStatement();
+            //JOptionPane.showMessageDialog(null, "Database connected");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Display table dish + elements in tabs
+        try{
+            String query1 = "Select * from dish where type='Appetizer'";
+            appetizerPanel = new JPanel();
+            appetizerPanel.setLayout(new BoxLayout(appetizerPanel, BoxLayout.Y_AXIS));
+            displayDishTable(query1, appetizerPanel);
+
+
+            String query2 = "Select * from dish where type='Dishes'";
+            dishPanel = new JPanel();
+            dishPanel.setLayout(new BoxLayout(dishPanel, BoxLayout.Y_AXIS));
+            displayDishTable(query2, dishPanel);
+
+
+            String query3 = "Select * from dish where type='Dessert'";
+            dessertPanel = new JPanel();
+            dessertPanel.setLayout(new BoxLayout(dessertPanel, BoxLayout.Y_AXIS));
+            displayDishTable(query3, dessertPanel);
+
+            String query4 = "Select * from dish where type='Drink'";
+            drinkPanel = new JPanel();
+            drinkPanel.setLayout(new BoxLayout(drinkPanel, BoxLayout.Y_AXIS));
+            displayDishTable(query4, drinkPanel);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
